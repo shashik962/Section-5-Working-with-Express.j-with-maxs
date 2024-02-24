20a523160639cb9e1ba74b0b82166efc9bbb998f
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-24 12:54:04 +0530

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/', (req, res, next) => {
+      console.log('This always run!');
+      next();
+});
+
+
 app.use(shopRoutes);
 app.use(adminRoutes);
 
 
-app.use('/', (req, res, next) => {
-      console.log('This always run!');
-      next();
+
+app.use((req, res, next) => {
+      res.status(404).send('<h1>Page Not Fund</h1>');
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-      console.log('In the another middleware here!');
       res.send('<h1>Hello from Express!</h1>');
 });
 
