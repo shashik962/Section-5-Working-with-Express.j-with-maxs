14c2fb113c5ec2ecaedc6b4a6471c4e09c657ba8
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-24 14:18:12 +0530

    Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 //admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-      console.log('In the another middleware here!');
-      res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+      res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 //admin/add-product => POST
