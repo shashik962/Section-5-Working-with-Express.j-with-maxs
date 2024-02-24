522e280e25434e5ad51684de42fd95602cc69710
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-24 12:32:14 +0530

    Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', (req, res, next) => {
       console.log('This always run!');
@@ -10,7 +12,12 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product', (req, res, next) => {
       console.log('In the another middleware here!');
-      res.send('<h1>The "Add Product" page</h1>');
+      res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+      console.log(req.body);
+      res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
