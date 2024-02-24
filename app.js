47b056eedef293f2f707614c0dcd80ebf2500adb
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-24 11:58:30 +0530

    Adding Middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+      console.log('In the middleware here!');
+      next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+      console.log('In the another middleware here!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
