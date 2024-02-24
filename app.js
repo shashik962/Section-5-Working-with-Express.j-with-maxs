990f6126d952cbf5d790714ddce9c0e22f01886b
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-24 16:12:08 +0530

    Serving Files Statically

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res, next) => {
       console.log('This always run!');
